0f9b8d70283466cc3b8e8abd74e96cf4dc2ecb4a
Siarhei Dudko <slavianich@gmail.com>
2018-02-18 14:50:05 +0300

    Смена структуры backend (добавление новой)

---
+++ backend/scripts/cert_send_toemail.php
@@ -0,0 +1,212 @@
+<?php
+// php /etc/openvpn/scripts/cert_send_toemail.php --args 1 1 admin@sergdudko.tk
+$servername = 'server'.$argv[2];
+$servernum = $argv[2];
+$usermail = $argv[4];
+$certificat = 'client'.$argv[3];
+
+/* загружаем настройки и настраиваем логгирование  */
+include(__DIR__ . '/settings.php'); 
+include(__DIR__ . '/cryptoAES.php');
+$current = 'php /etc/openvpn/scripts/cert_send_toemail.php --args '.$argv[2].' '.$argv[3].' '.$argv[4] . PHP_EOL;
+if(!file_exists('/etc/openvpn/dudko-web-panel/')) { 
+  if(!mkdir('/etc/openvpn/dudko-web-panel/', 0755, true)) {
+    echo 'Не удалось создать каталог .../dudko-web-panel/!';
+    exit;
+  }
+}
+if(!is_dir('/etc/openvpn/dudko-web-panel/logs/')){ mkdir('/etc/openvpn/dudko-web-panel/logs/');}
+$file = '/etc/openvpn/dudko-web-panel/logs/'.$servername.date("Y-m-d").'.log';
+if(file_exists($file)){ $current .= file_get_contents($file); }
+/* проверяем, что аргументы получены, требуемый сертификат имеется на сервере и  не принадлежит серверу */ 
+if(!isset($usermail) || !isset($certificat)|| !isset($servername)){
+  	echo 'Некорректные параметры для отправки!';
+  	exit;
+} 
+if(!file_exists('/etc/openvpn/dudko-web-panel/rsa-key/'.$servername.'/'.$certificat.'.key') || !file_exists('/etc/openvpn/dudko-web-panel/rsa-key/'.$servername.'/'.$certificat.'.crt')){ 
+  	echo 'Сертификаты для данного клиента отсутствуют!';
+  	exit; 
+}
+
+/* получаем переменные из БД для авторизации на почтовом сервере */
+GetPostConf($db_key, $db_user, $db_pass, $db_projectname, $variable);
+
+/* создаем архив с файлами  */
+//генерация пароля на архив
+$passwd = ''; 
+$array = array_merge(range('A','Z'),range('a','z'),range('0','9')); 
+$c = count($array); 
+$longpass = rand(70,100);
+for($i=0;$i<$longpass;$i++) {$passwd .= $array[rand(0,$c-1)];}
+//создадим временный файл в памяти
+$zip_file = '/tmp/zip-cert-'.md5(time()).'.zip';
+$config_file = '/tmp/'.$certificat.'.ovpn';
+file_put_contents($config_file, generate_config($servernum, $certificat)); 
+//вариант через shell(с паролем)
+$files_to_zip = '';
+if(file_exists('/etc/openvpn/dudko-web-panel/rsa-key/'.$servername.'/'.$certificat.'.key')){ 
+  $files_to_zip .= $certificat.'.key ';
+}
+if(file_exists('/etc/openvpn/dudko-web-panel/rsa-key/'.$servername.'/'.$certificat.'.crt')){ 
+  $files_to_zip .= $certificat.'.crt ';
+}
+if(file_exists('/etc/openvpn/dudko-web-panel/rsa-key/'.$servername.'/ta.key')){ 
+  $files_to_zip .= 'ta.key ';
+}
+if(file_exists('/etc/openvpn/dudko-web-panel/rsa-key/'.$servername.'/ca.crt')){ 
+  $files_to_zip .= 'ca.crt ';
+}
+$answr = explode(' ', exec('cd  /etc/openvpn/dudko-web-panel/rsa-key/'.$servername.'/ && zip '.$zip_file.' '.$files_to_zip.' -P '.$passwd));
+if ($answr[2] != 'adding:') {
+    $current .= date("Y-m-d H:i:s").'     '."Ошибка создания архива";
+  	$current .= PHP_EOL;
+  	unlink($zip_file);
+	unlink($config_file);
+  	file_put_contents($file, $current);  
+  	exit;
+}
+if(file_exists($config_file)){ 
+  $files_to_zip = $certificat.'.ovpn';
+}
+$answr = explode(' ', exec('cd  /tmp/ && zip '.$zip_file.' '.$files_to_zip.' -P '.$passwd));
+if ($answr[2] != 'adding:') {
+    $current .= date("Y-m-d H:i:s").'     '."Ошибка добавления конфиг-файла в архив";
+  	$current .= PHP_EOL;
+  	unlink($zip_file);
+	unlink($config_file);
+  	file_put_contents($file, $current);  
+  	exit;
+}
+
+/* отправка архива на почту  */
+$_SERVER["SERVER_NAME"] = 'sergdudko.tk';
+require_once ("/etc/openvpn/scripts/SendMailSmtpClass.php"); // подключаем класс
+$mailSMTP = new SendMailSmtpClass($variable['email_user'], MyCryptoAES(md5($db_user.'vpnsergdudkotk').md5($db_pass.'vpnsergdudkotk'), 'email_pass', $variable['email_pass'], 'decrypt'), $variable['email_host'], $variable['email_name'], $variable['email_port']); // создаем экземпляр класса
+$headers= "MIME-Version: 1.0\r\n"; 
+$un = rand(1000,9999);
+//$headers .= "Content-Type: multipart/alternative;boundary=\"----------".$un."\"".PHP_EOL; // кодировка письма
+$headers .= "Content-Type: multipart/mixed;boundary=\"----------".$un."\"".PHP_EOL; // кодировка письма
+$headers .= "Content-Language: ru". PHP_EOL;
+$headers .= 'From: '.$variable['email_name'].' <'.$variable['email_user'].'>'.PHP_EOL; // от кого письмо
+$headers .= 'To: '.$usermail.PHP_EOL; // от кого письмо
+$message = 	"------------".$un. PHP_EOL .
+  			"Content-Type:text/plain; charset=utf-8". PHP_EOL .
+  			"Content-Transfer-Encoding: 8bit". PHP_EOL .
+  			"Архив с сертификатами во вложении, пароль на архив был предоставлен на сайте.". PHP_EOL .
+  			"Обращаю внимание на то, что длинна не все архиваторы поддерживают заданную длинну пароля. Если вы получили сообщение о том, что пароль не верен - попробуйте другим архиватором.". PHP_EOL .
+"------------".$un. PHP_EOL .
+  			"Content-Type: application/octet-stream;name=\"file.zip\"". PHP_EOL .
+  			"Content-Transfer-Encoding:base64". PHP_EOL .
+  			"Content-Disposition:attachment;filename=\"file.zip\"". PHP_EOL .chunk_split(base64_encode(file_get_contents($zip_file))). PHP_EOL ."
+------------".$un."--";
+$subject = 'Ключи доступа'.PHP_EOL;
+$result =  $mailSMTP->send($usermail, $subject, $message, $headers); // отправляем письмо
+if($result === true){
+  echo 'Сертификаты были успешно отправлены на '. $usermail . ', пароль на архив:' . PHP_EOL . $passwd .PHP_EOL; 
+  $current .= date("Y-m-d H:i:s").'     '.'Сертификаты были успешно отправлены на '. $usermail .PHP_EOL;
+}else{
+  $current .= date("Y-m-d H:i:s").'     '.'Попытка отправить сертификаты через резервный ящик!'.PHP_EOL;
+  $mailSMTP_reserve = new SendMailSmtpClass($variable['email_user_reserve'], $variable['email_pass_reserve'], MyCryptoAES(md5($db_user.'vpnsergdudkotk').md5($db_pass.'vpnsergdudkotk'), 'email_host_reserve', $variable['email_host_reserve'], 'decrypt'), $variable['email_name'], $variable['email_port_reserve']); // создаем экземпляр класса
+  $result2 =  $mailSMTP_reserve->send($usermail, $subject, $message, $headers); // отправляем письмо с резервного ящика
+  if($result2 === true){
+    echo 'Сертификаты были успешно отправлены на '. $usermail . ', пароль на архив:' . PHP_EOL . $passwd .PHP_EOL; 
+    $current .= date("Y-m-d H:i:s").'     '.'Сертификаты были успешно отправлены на '. $usermail .PHP_EOL;
+  }else{
+    echo 'Ошибка отправки сертификатов на '. $usermail . '!' . PHP_EOL; 
+    $current .= date("Y-m-d H:i:s").'     '.'Ошибка отправки сертификатов на '. $usermail . '!' . PHP_EOL;    
+  }
+}
+
+/* Удаляем архив и пишем содержимое лога в файл */
+unlink($zip_file);
+unlink($config_file);
+$current .= PHP_EOL;
+file_put_contents($file, $current);
+
+function generate_config($server_num, $certificat){
+  	include(__DIR__ . '/settings.php'); 
+    GetServerConf($db_key, $db_user, $db_pass, $server_num, $db_projectname, $variable_func);
+	$variable_func['port'] = 1700+intval($variable_func['Id']);
+  	$conf_file = '';
+    $conf_file .= 'tls-client'.PHP_EOL;
+    $conf_file .= 'proto tcp-client'.PHP_EOL;
+  	$conf_file .= 'remote '.$variable_func['hostname'].PHP_EOL;
+  	$conf_file .= 'dev '.$variable_func['dev'].PHP_EOL;
+	$conf_file .= 'port '.$variable_func['port'].PHP_EOL;
+  	$conf_file .= 'pull '.PHP_EOL;
+    $conf_file .= 'tls-auth ta.key 1'.PHP_EOL;
+  	$conf_file .= 'ca ca.crt'.PHP_EOL;
+	$conf_file .= 'cert '.$certificat.'.crt'.PHP_EOL;
+  	$conf_file .= 'key '.$certificat.'.key'.PHP_EOL;
+  	$conf_file .= 'cipher '.$variable_func['cipher'].PHP_EOL;
+  	$conf_file .= 'comp-lzo'.PHP_EOL;
+  	return $conf_file;
+}
+
+function GetPostConf($db_key_this, $db_user_this, $db_pass_this, $db_projectname_this, &$variable_this){
+	$user_token = GetFirebaseUserToken($db_key_this, $db_user_this, $db_pass_this);
+	if($user_token == 'auth_firebase_error'){
+		echo 'Ошибка авторизации в базе данных.';
+		exit;
+	}
+	$url = 'https://'.$db_projectname_this.'.firebaseio.com/'.$user_token[1].'.json?auth='.$user_token[0];
+
+	$ch = curl_init($url);                                                                      
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");                                                                                                                                      
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                                                                                                                                      
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+		 'Content-Type: application/json',)                                                                  
+		);  	
+	$result = curl_exec($ch);
+	try {
+		$result_arr = json_decode($result, true);
+		$variable_this = $result_arr['adminmail'];
+	} catch(Exception $e) {
+	}
+}
+
+function GetServerConf($db_key_this, $db_user_this, $db_pass_this, $id_server_this, $db_projectname_this, &$variable_this){
+	$user_token = GetFirebaseUserToken($db_key_this, $db_user_this, $db_pass_this);
+	if($user_token == 'auth_firebase_error'){
+		echo 'Ошибка авторизации в базе данных.';
+		exit;
+	}
+	$url = 'https://'.$db_projectname_this.'.firebaseio.com/'.$user_token[1].'.json?auth='.$user_token[0];
+
+	$ch = curl_init($url);                                                                      
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");                                                                                                                                      
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                                                                                                                                      
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+		 'Content-Type: application/json',)                                                                  
+		);  	
+	$result = curl_exec($ch);
+	try {
+		$result_arr = json_decode($result, true);
+		$variable_this = $result_arr['server'][$id_server_this];
+		if(isset($variable_this['dev'])){
+			$variable_this['Id'] = $id_server_this;
+		}
+	} catch(Exception $e) {
+	}
+}
+
+function GetFirebaseUserToken($db_key_this, $db_user_this, $db_pass_this){
+	$url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key='.$db_key_this;
+	$json = '{"email": "'.$db_user_this.'","password": "'.$db_pass_this.'","returnSecureToken": true}';
+	$ch = curl_init($url);                                                                      
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $json);                                                                  
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                                                                                                                                      
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+		 'Content-Type: application/json',)                                                                  
+		);  	
+	$result = curl_exec($ch);
+	try {
+		$result_arr = json_decode($result, true);
+		return [$result_arr['idToken'], $result_arr['localId']];
+	} catch(Exception $e) {
+		return 'auth_firebase_error';
+	}
+}
+exit;
+?>
---
+++ backend/scripts/core_web.php
@@ -0,0 +1,134 @@
+<?php
+$content = file_get_contents("php://input");
+$command = json_decode($content, TRUE);
+header("Content-type: text/txt; charset=utf-8");
+
+/* проверка имени пользователя и пароля */
+include(__DIR__ . '/settings.php'); 
+if(($command['auth']['username'] != md5($db_user.'vpnsergdudkotk')) || ($command['auth']['password'] != md5($db_pass.'vpnsergdudkotk'))){
+  	echo 'Авторизация не пройдена!' . PHP_EOL;
+  	exit;
+}
+
+if(isset($command['build'])){
+    $exec = 'php /etc/openvpn/scripts/build_'.$command['build'].'.php --args ';
+    if(!isset($command['server']) || !isset($command['com'])){
+		helper();
+    }
+    if($command['build'] == 'server'){
+        $exec .= '-'.$command['com'] . ' ' . $command['server'];
+    }
+    if($command['build'] == 'client'){
+      	if(!isset($command['client'])){
+            helper();
+        }
+      	if($command['com'] != 'remove'){
+          	if(!isset($command['email'])){
+              	helper();
+            }
+        }
+     	$exec .= '-'.$command['com'] . ' ' . $command['server'] . ' ' . $command['client'];
+      	if(isset($command['email'])){
+          	 $exec .= ' ' . $command['email'];
+        }
+    }
+}
+
+if(isset($command['send'])){
+  if($command['send'] == 'toemail'){
+    	if(!isset($command['server']) || !isset($command['email']) || !isset($command['client'])){
+            helper();
+        }
+        $exec = 'php /etc/openvpn/scripts/cert_send_'.$command['send'].'.php --args '.$command['server'].' '.$command['client'].' '.$command['email'].'';
+  }
+}
+
+if(isset($exec)){
+	exec($exec, $callback);
+  	for($i=0;$i<count($callback);$i++){
+      	echo $callback[$i];
+      	if(($i+1)<count($callback)){
+          	echo PHP_EOL;
+        }
+    }
+  	echo PHP_EOL;
+} elseif(isset($command['settings'])){
+    settings($command);
+} elseif(isset($command['status'])){
+    status($command);
+} else {
+    helper();
+}
+
+function helper(){
+    echo 'Доступные аргументы:' .PHP_EOL;
+  	echo 'Вызов справки: -help' .PHP_EOL;
+    echo 'Создать/удалить openvpn сервер: build:server com:add(или remove) server:1(численный номер сервера)' .PHP_EOL;
+    echo 'Создать/удалить сертификаты клиента: build:client com:add(или remove) server:1(численный номер сервера, к которому будет привязан клиент)  email:admin@sergdudko.tk  client:1(численный номер клиентского сертификата)' .PHP_EOL;
+    echo 'Отправить сертификаты клиента на email: send:toemail(отправка на email) server:1(численный номер сервера, к которому будет привязан клиент)  email:admin@sergdudko.tk  client:1(численный номер клиентского сертификата)' .PHP_EOL;
+  	echo 'Для настройки программы, наберите settings: {db_host:hostname(адрес БД MySQL или ip) db_name:name(имя БД) db_user:user(пользователь БД) db_pass:password(пароль пользователя БД) encoding:Europe/Minsk(таймзона)}' .PHP_EOL;
+    echo PHP_EOL;
+    exit;
+}
+
+function settings($command){
+   $settings_file = "/etc/openvpn/scripts/settings.php";
+   if(!file_exists($settings_file)){
+     	echo 'Файл с настройками не найден!' . PHP_EOL;
+     	exit;
+   }
+   $handle = @fopen($settings_file, "r");
+   if ($handle) {
+     	$i=0;
+        while (($buffer = fgets($handle, 4096)) !== false) {
+            $str[$i] = $buffer;
+            $i++;
+        }
+        if (!feof($handle)) {
+            echo "Error: unexpected fgets() fail\n";
+        }
+   		fclose($handle);
+   }
+   $current = '';
+   $flag = 0;
+   for($i=0;$i<count($str);$i++){
+       if(isset($command['settings']['db_key']) && (substr($str[$i], 0, 7) == '$db_key')){
+			$str[$i] = '$db_key = \''.$command['settings']['db_key'].'\';       //токен проекта firebase' . PHP_EOL;
+			$flag = 1;
+		}
+       if(isset($command['settings']['db_projectname']) && (substr($str[$i], 0, 15) == '$db_projectname')){
+			$str[$i] = '$db_projectname = \''.$command['settings']['db_projectname'].'\';       //название проекта firebase' . PHP_EOL;
+			$flag = 1;
+		}
+       if(isset($command['settings']['db_user']) && (substr($str[$i], 0, 8) == '$db_user')){
+			$str[$i] = '$db_user = \''.$command['settings']['db_user'].'\';         //пользователь firebase' . PHP_EOL;
+         	$flag = 1;
+       } 
+       if(isset($command['settings']['db_pass']) && (substr($str[$i], 0, 8) == '$db_pass')){
+			$str[$i] = '$db_pass = \''.$command['settings']['db_pass'].'\';         //пароль' . PHP_EOL;
+         	$flag = 1;
+       }  
+       if(isset($command['settings']['timezone']) && (substr($str[$i], 0, 25) == 'date_default_timezone_set')){
+			$str[$i] = 'date_default_timezone_set(\''.$command['settings']['timezone'].'\');' . PHP_EOL;
+         	$flag = 1;
+       }
+       $current .= $str[$i];
+   }
+   if($flag == 1) {
+     	if(file_put_contents($settings_file, $current)){
+          	echo 'Настройки изменены!' . PHP_EOL;
+        }
+   } else {
+     	echo 'Параметры: db_key:ключ API проекта(firebase) db_user:user(пользователь БД) db_pass:password(пароль пользователя БД) encoding:Europe/Minsk(таймзона)' . PHP_EOL;
+     	echo 'settings: обязательный префикс, лишние параметры можно пропустить' . PHP_EOL;
+   }
+   exit;
+}
+
+function status($command){
+   echo exec('systemctl status openvpn@server'.$command['status']['server'].'.service |grep Active');
+   exit;
+}
+
+exit;
+?>
