b905c0fc3be470f996885896ae13fcd8bcdf24d1
Siarhei Dudko <slavianich@gmail.com>
2018-02-23 21:55:46 +0300

    замена релиза

--- backend/scripts/settings.php
+++ backend/scripts/settings.php
@@ -8,8 +8,8 @@ https://github.com/siarheidudko/dwpanel/LICENSE
 
 $db_key = 'AIzaSyBPBoZyDD9Q51aiRjyL1mNjgAyUfQeEedc';       //токен firebase
 $db_projectname = 'vpnsergdudkotk';       //название проекта firebase
-$db_user = 'admin@sergdudko.tk';         //пользователь firebase
-$db_pass = '999121';         //пароль
+$db_user = 'testuser@sergdudko.tk';         //пользователь firebase
+$db_pass = 'password';         //пароль
 
 date_default_timezone_set('Europe/Minsk');
 header('Content-Type: text/text; charset=utf-8');
